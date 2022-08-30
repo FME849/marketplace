5a17dc7d437521c43672c68538774e71d106c0da
FME849 <1414439@gmail.com>
2022-08-30 14:24:44 +0700

    Rename stacks folder

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { Provider as MetacraftProvider, themeState } from '@metacraft/ui';
 import { NavigationContainer } from '@react-navigation/native';
 import { useSnapshot } from 'valtio';
 
-import MarketplaceStack from './stack';
+import MarketplaceStack from './stacks';
 
 export const AppContainer: FC = () => {
 	const theme = useSnapshot(themeState);
