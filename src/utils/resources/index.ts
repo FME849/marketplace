f485ae0b663be4aa93db67daf4766cd8efbe0426
FME849 <1414439@gmail.com>
2022-08-30 15:09:19 +0700

    Add visual resources to use in ButtonFantasy component

---
+++ src/utils/resources/index.ts
@@ -0,0 +1,7 @@
+export default {
+	buttonFantasy: {
+		leftEdge: require('../../../assets/visual/button-left-edge.png'),
+		rightEdge: require('../../../assets/visual/button-right-edge.png'),
+		middleBackground: require('../../../assets/visual/button-bg.png'),
+	},
+};
---
+++ src/utils/resources/index.web.ts
@@ -0,0 +1,7 @@
+export default {
+	buttonFantasy: {
+		leftEdge: { uri: '/visual/button-left-edge.png' },
+		rightEdge: { uri: '/visual/button-right-edge.png' },
+		middleBackground: { uri: '/visual/button-bg.png' },
+	},
+};
